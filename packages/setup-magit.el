eb475e7f883b78a66f83b762ac06c00271fc5f07
boosja <mathias.iversen487@gmail.com>
2025-12-29 15:52:23 +0100

    Add magit conveniences

--- packages/setup-magit.el
+++ packages/setup-magit.el
@@ -7,6 +7,10 @@
                                             (unpulled . show)
                                             (stashes . show)))
   (magit-diff-refine-hunk t)
+  (magit-push-always-verify nil) ;; does not exist?
+  (magit-revert-buffers 'silent)
+  (magit-no-confirm '(stage-all-changes
+                      unstage-all-changes))
 
   :bind (("C-x m" . magit-status)
          (:map magit-status-mode-map
@@ -14,6 +18,41 @@
 
   :config
   (wrap-fullscreen magit-status)
-  (wrap-fullscreen magit-init))
+  (wrap-fullscreen magit-init)
+
+  ;; move cursor into position when entering commit message
+  (add-hook 'git-commit-mode-hook 'my/magit-cursor-fix))
+
+(defun kill-magit-buffers ()
+  (let ((current (current-buffer)))
+    (dolist (buf (magit-mode-get-buffers))
+      (unless (eq buf current)
+        (kill-buffer buf)))))
+
+(defun magit-quit ()
+  "Like magit-mode-bury-buffer, but also restores the window
+configuration stored by magit-status-fullscreen"
+  (interactive)
+  (let ((prev my/previous-window-configuration))
+    (kill-magit-buffers)
+    (funcall magit-bury-buffer-function 'kill-buffer)
+    (when prev (register-val-jump-to prev nil))))
+
+(defun my/magit-cursor-fix ()
+  (beginning-of-buffer)
+  (when (looking-at "#")
+    (while (looking-at "#")
+      (forward-line))
+    (forward-line)))
+
+(defun b-a-r-k ()
+  (interactive)
+  (browse-at-remote-kill)
+  (message "Remote was killed 💀"))
+
+(use-package browse-at-remote
+  :defer t
+  :custom (browse-at-remote-prefer-symbolic t)
+  :bind (("C-x v w" . b-a-r-k)))
 
 (provide 'setup-magit)
