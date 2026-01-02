00178a67927d4b19f6c566b942c45c511313fce5
boosja <mathias.iversen487@gmail.com>
2026-01-02 20:43:45 +0100

    Add keybinding to cycle windows

--- settings/navigation.el
+++ settings/navigation.el
@@ -4,3 +4,8 @@
 (global-set-key (kbd "S-<left>") 'windmove-left)
 (global-set-key (kbd "S-<up>") 'windmove-up)
 (global-set-key (kbd "S-<down>") 'windmove-down)
+
+;; Cycle windows
+(global-set-key (kbd "C-M-S-s-j") 'other-window)
+
+(provide 'navigation)
