358e69cffd84f8524020052195943b6f453d7c7f
boosja <mathias.iversen487@gmail.com>
2026-01-03 09:04:30 +0100

    Split the windows right

--- settings/navigation.el
+++ settings/navigation.el
@@ -8,4 +8,26 @@
 ;; Cycle windows
 (global-set-key (kbd "C-M-S-s-j") 'other-window)
 
+;; Keep them windows nice and balanced
+(global-set-key (kbd "C-x 0") 'my/delete-window)
+(global-set-key (kbd "C-x 2") 'my/split-window-down)
+(global-set-key (kbd "C-x 3") 'my/split-window-right)
+
+(defun my/delete-window ()
+  (interactive)
+  (delete-window)
+  (balance-windows))
+
+(defun my/split-window-down ()
+  (interactive)
+  (split-window-below)
+  (windmove-down)
+  (balance-windows))
+
+(defun my/split-window-right ()
+  (interactive)
+  (split-window-right)
+  (windmove-right)
+  (balance-windows))
+
 (provide 'navigation)
